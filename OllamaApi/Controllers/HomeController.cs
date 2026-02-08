9076dc675b475fb7b98b107ff36033de3cae1dd0
Kevin Siraki <kevsiraki@gmail.com>
2026-02-08 14:24:54 -0800

    Add a little home endpoint :).

---
+++ OllamaApi/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OllamaApi.Attributes;
+using OllamaApi.Models;
+using OllamaApi.Services;
+
+namespace OllamaApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Route("api/")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ChatService _service;
+
+        public HomeController()
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Home(ChatRequest request)
+        {
+            return Ok("AI Gateway Services Are Running.");
+        }
+    }
+}
